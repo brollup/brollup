1117bfe90a9d6cacbaf766863b1600c58e1b2795
brqgoo <100725376+brqgoo@users.noreply.github.com>
2025-01-06 18:26:34 +0300

    Update noist.rs

--- tests/noist.rs
+++ tests/noist.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod noist_tests {
-    use brollup::into::IntoPointVec;
+    use brollup::into::{IntoPointVec, IntoScalar};
     use brollup::noist::dkg::package::DKGPackage;
     use brollup::noist::dkg::session::DKGSession;
 
@@ -72,6 +72,7 @@ mod noist_tests {
         assert!(vse_setup.insert(signer_3_auth_keymap));
         assert!(vse_setup.validate());
 
+        // Signer 1 DKG package.
         let package_1 = DKGPackage::new(signer_1_secret, &full_list).unwrap();
         assert!(package_1.is_complete(&full_list));
         assert!(package_1.vss_verify());
@@ -79,6 +80,7 @@ mod noist_tests {
         let auth_package_1 = Authenticable::new(package_1, signer_1_secret).unwrap();
         assert!(auth_package_1.authenticate());
 
+        // Signer 2 DKG package.
         let package_2 = DKGPackage::new(signer_2_secret, &full_list).unwrap();
         assert!(package_2.is_complete(&full_list));
         assert!(package_2.vss_verify());
@@ -86,6 +88,7 @@ mod noist_tests {
         let auth_package_2 = Authenticable::new(package_2, signer_2_secret).unwrap();
         assert!(auth_package_2.authenticate());
 
+        // Signer 3 DKG package.
         let package_3 = DKGPackage::new(signer_3_secret, &full_list).unwrap();
         assert!(package_3.is_complete(&full_list));
         assert!(package_3.vss_verify());
@@ -98,7 +101,6 @@ mod noist_tests {
         assert!(session.insert(&auth_package_1, &vse_setup));
         assert!(session.insert(&auth_package_2, &vse_setup));
         assert!(session.insert(&auth_package_3, &vse_setup));
-
         assert!(session.is_above_threshold());
         assert!(session.verify(&vse_setup));
 
@@ -110,7 +112,7 @@ mod noist_tests {
             .group_combined_post_binding_point(None, None)
             .unwrap();
 
-        let _combined_group_point = session.group_combined_full_point(None, None).unwrap();
+        let combined_group_point = session.group_combined_full_point(None, None).unwrap();
 
         let s_1_hiding_secret = session
             .signatory_combined_hiding_secret(signer_1_secret)
@@ -296,6 +298,255 @@ mod noist_tests {
             s1_s2_s3_combined_post_binding_secret_lagranced.base_point_mul(),
             combined_group_post_binding_point
         );
+
+        // Create a new session for the nonce:
+
+        // Signer 1 DKG package for nonce.
+        let package_1_nonce = DKGPackage::new(signer_1_secret, &full_list).unwrap();
+        assert!(package_1_nonce.is_complete(&full_list));
+        assert!(package_1_nonce.vss_verify());
+        assert!(package_1_nonce.vse_verify(&vse_setup));
+        let auth_package_1_nonce = Authenticable::new(package_1_nonce, signer_1_secret).unwrap();
+        assert!(auth_package_1_nonce.authenticate());
+
+        // Signer 2 DKG package for nonce.
+        let package_2_nonce = DKGPackage::new(signer_2_secret, &full_list).unwrap();
+        assert!(package_2_nonce.is_complete(&full_list));
+        assert!(package_2_nonce.vss_verify());
+        assert!(package_2_nonce.vse_verify(&vse_setup));
+        let auth_package_2_nonce = Authenticable::new(package_2_nonce, signer_2_secret).unwrap();
+        assert!(auth_package_2_nonce.authenticate());
+
+        // Signer 3 DKG package for nonce.
+        let package_3_nonce = DKGPackage::new(signer_3_secret, &full_list).unwrap();
+        assert!(package_3_nonce.is_complete(&full_list));
+        assert!(package_3_nonce.vss_verify());
+        assert!(package_3_nonce.vse_verify(&vse_setup));
+        let auth_package_3_nonce = Authenticable::new(package_3_nonce, signer_3_secret).unwrap();
+        assert!(auth_package_3_nonce.authenticate());
+
+        let mut session_nonce = DKGSession::new(1, &full_list).unwrap();
+
+        assert!(session_nonce.insert(&auth_package_1_nonce, &vse_setup));
+        assert!(session_nonce.insert(&auth_package_2_nonce, &vse_setup));
+        assert!(session_nonce.insert(&auth_package_3_nonce, &vse_setup));
+        assert!(session_nonce.is_above_threshold());
+        assert!(session_nonce.verify(&vse_setup));
+
+        // combined_group_hiding_point
+        // combined_group_post_binding_point
+
+        let gk = Some(combined_group_point.serialize_xonly());
+        let msg = Some([0xffu8; 32]);
+
+        let combined_group_point_nonce = session_nonce.group_combined_full_point(gk, msg).unwrap();
+
+        let s_1_hiding_secret_nonce = session_nonce
+            .signatory_combined_hiding_secret(signer_1_secret)
+            .unwrap();
+        let s_1_hiding_public_nonce = session_nonce
+            .signatory_combined_hiding_point(signer_1_public)
+            .unwrap();
+        let s_1_post_binding_secret_nonce = session_nonce
+            .signatory_combined_post_binding_secret(signer_1_secret, gk, msg)
+            .unwrap();
+        let s_1_post_binding_public_nonce = session_nonce
+            .signatory_combined_post_binding_point(signer_1_public, gk, msg)
+            .unwrap();
+
+        let s_2_hiding_secret_nonce = session_nonce
+            .signatory_combined_hiding_secret(signer_2_secret)
+            .unwrap();
+        let s_2_hiding_public_nonce = session_nonce
+            .signatory_combined_hiding_point(signer_2_public)
+            .unwrap();
+        let s_2_post_binding_secret_nonce = session_nonce
+            .signatory_combined_post_binding_secret(signer_2_secret, gk, msg)
+            .unwrap();
+        let s_2_post_binding_public_nonce = session_nonce
+            .signatory_combined_post_binding_point(signer_2_public, gk, msg)
+            .unwrap();
+
+        let s_3_hiding_secret_nonce = session_nonce
+            .signatory_combined_hiding_secret(signer_3_secret)
+            .unwrap();
+        let s_3_hiding_public_nonce = session_nonce
+            .signatory_combined_hiding_point(signer_3_public)
+            .unwrap();
+        let s_3_post_binding_secret_nonce = session_nonce
+            .signatory_combined_post_binding_secret(signer_3_secret, gk, msg)
+            .unwrap();
+        let s_3_post_binding_public_nonce = session_nonce
+            .signatory_combined_post_binding_point(signer_3_public, gk, msg)
+            .unwrap();
+
+        // challenge
+        let challenge = [0x05u8; 32].into_scalar().unwrap();
+
+        let group_key_parity = combined_group_point.parity();
+        let group_nonce_parity = combined_group_point_nonce.parity();
+
+        let group_key_is_odd: bool = group_key_parity.into();
+        let group_nonce_is_odd: bool = group_nonce_parity.into();
+
+        println!("group_key_is_odd : {}", group_key_is_odd);
+        println!("group_nonce_is_odd : {}", group_nonce_is_odd);
+
+        let combined_group_point_ = combined_group_point.negate_if(combined_group_point.parity());
+        let combined_group_point_nonce_ =
+            combined_group_point_nonce.negate_if(combined_group_point_nonce.parity());
+
+        // Signer 1 partial signing:
+        let s_1_hiding_secret_ = s_1_hiding_secret.negate_if(group_key_parity);
+        let s_1_post_binding_secret_ = s_1_post_binding_secret.negate_if(group_key_parity);
+        let s_1_hiding_public_ = s_1_hiding_point.negate_if(group_key_parity);
+        let s_1_post_binding_public_ = s_1_post_binding_point.negate_if(group_key_parity);
+
+        let s_1_hiding_secret_nonce_ = s_1_hiding_secret_nonce.negate_if(group_nonce_parity);
+        let s_1_post_binding_secret_nonce_ =
+            s_1_post_binding_secret_nonce.negate_if(group_nonce_parity);
+        let s_1_hiding_public_nonce_ = s_1_hiding_public_nonce.negate_if(group_nonce_parity);
+        let s_1_post_binding_public_nonce_ =
+            s_1_post_binding_public_nonce.negate_if(group_nonce_parity);
+
+        // (k + ed) + (k + ed)
+        let s1_partial_sig_ = s_1_hiding_secret_nonce_
+            + (challenge * s_1_hiding_secret_)
+            + s_1_post_binding_secret_nonce_
+            + (challenge * s_1_post_binding_secret_);
+
+        let s1_partial_sig = s1_partial_sig_.unwrap();
+
+        // Signer 1 partial signature verification:
+        // (R + eP) + (R + eP)
+        let equation_ = s_1_hiding_public_nonce_
+            + (challenge * s_1_hiding_public_)
+            + s_1_post_binding_public_nonce_
+            + (challenge * s_1_post_binding_public_);
+
+        let equation = equation_.unwrap();
+
+        assert_eq!(s1_partial_sig.base_point_mul(), equation);
+
+        // Signer 2 partial signing:
+        let s_2_hiding_secret_ = s_2_hiding_secret.negate_if(group_key_parity);
+        let s_2_post_binding_secret_ = s_2_post_binding_secret.negate_if(group_key_parity);
+        let s_2_hiding_public_ = s_2_hiding_point.negate_if(group_key_parity);
+        let s_2_post_binding_public_ = s_2_post_binding_point.negate_if(group_key_parity);
+
+        let s_2_hiding_secret_nonce_ = s_2_hiding_secret_nonce.negate_if(group_nonce_parity);
+        let s_2_post_binding_secret_nonce_ =
+            s_2_post_binding_secret_nonce.negate_if(group_nonce_parity);
+        let s_2_hiding_public_nonce_ = s_2_hiding_public_nonce.negate_if(group_nonce_parity);
+        let s_2_post_binding_public_nonce_ =
+            s_2_post_binding_public_nonce.negate_if(group_nonce_parity);
+
+        // (k + ed) + (k + ed)
+        let s2_partial_sig_ = s_2_hiding_secret_nonce_
+            + (challenge * s_2_hiding_secret_)
+            + s_2_post_binding_secret_nonce_
+            + (challenge * s_2_post_binding_secret_);
+
+        let s2_partial_sig = s2_partial_sig_.unwrap();
+
+        // Signer 2 partial signature verification:
+        // (R + eP) + (R + eP)
+        let equation_ = s_2_hiding_public_nonce_
+            + (challenge * s_2_hiding_public_)
+            + s_2_post_binding_public_nonce_
+            + (challenge * s_2_post_binding_public_);
+
+        let equation = equation_.unwrap();
+
+        assert_eq!(s2_partial_sig.base_point_mul(), equation);
+
+        // Signer 3 partial signing:
+        let s_3_hiding_secret_ = s_3_hiding_secret.negate_if(group_key_parity);
+        let s_3_post_binding_secret_ = s_3_post_binding_secret.negate_if(group_key_parity);
+        let s_3_hiding_public_ = s_3_hiding_point.negate_if(group_key_parity);
+        let s_3_post_binding_public_ = s_3_post_binding_point.negate_if(group_key_parity);
+
+        let s_3_hiding_secret_nonce_ = s_3_hiding_secret_nonce.negate_if(group_nonce_parity);
+        let s_3_post_binding_secret_nonce_ =
+            s_3_post_binding_secret_nonce.negate_if(group_nonce_parity);
+        let s_3_hiding_public_nonce_ = s_3_hiding_public_nonce.negate_if(group_nonce_parity);
+        let s_3_post_binding_public_nonce_ =
+            s_3_post_binding_public_nonce.negate_if(group_nonce_parity);
+
+        // (k + ed) + (k + ed)
+        let s3_partial_sig_ = s_3_hiding_secret_nonce_
+            + (challenge * s_3_hiding_secret_)
+            + s_3_post_binding_secret_nonce_
+            + (challenge * s_3_post_binding_secret_);
+
+        let s3_partial_sig = s3_partial_sig_.unwrap();
+
+        // Signer 1 partial signature verification:
+        // (R + eP) + (R + eP)
+        let equation_ = s_3_hiding_public_nonce_
+            + (challenge * s_3_hiding_public_)
+            + s_3_post_binding_public_nonce_
+            + (challenge * s_3_post_binding_public_);
+
+        let equation = equation_.unwrap();
+
+        assert_eq!(s3_partial_sig.base_point_mul(), equation);
+
+        println!("buraya kadar :)");
+
+        // Case #1 signer 1 & 2 produced.
+        let active_list = vec![signer_1_public, signer_2_public];
+        let index_list = lagrance_index_list(&full_list, &active_list).unwrap();
+
+        let s1_index = lagrance_index(&full_list, signer_1_public).unwrap();
+        let s1_lagrance = interpolating_value(&index_list, s1_index).unwrap();
+
+        let s2_index = lagrance_index(&full_list, signer_2_public).unwrap();
+        let s2_lagrance = interpolating_value(&index_list, s2_index).unwrap();
+
+        let s1_s2_agg_sig_ = (s1_partial_sig * s1_lagrance) + (s2_partial_sig * s2_lagrance);
+        let s1_s2_agg_sig = s1_s2_agg_sig_.unwrap();
+
+        let equation_ = combined_group_point_nonce_ + (challenge * combined_group_point_);
+        let equation = equation_.unwrap();
+
+        assert_eq!(s1_s2_agg_sig.base_point_mul(), equation);
+
+        // Case #2 signer 1 & 3 produced.
+        let active_list = vec![signer_1_public, signer_3_public];
+        let index_list = lagrance_index_list(&full_list, &active_list).unwrap();
+
+        let s1_index = lagrance_index(&full_list, signer_1_public).unwrap();
+        let s1_lagrance = interpolating_value(&index_list, s1_index).unwrap();
+
+        let s3_index = lagrance_index(&full_list, signer_3_public).unwrap();
+        let s3_lagrance = interpolating_value(&index_list, s3_index).unwrap();
+
+        let s1_s3_agg_sig_ = (s1_partial_sig * s1_lagrance) + (s3_partial_sig * s3_lagrance);
+        let s1_s3_agg_sig = s1_s3_agg_sig_.unwrap();
+
+        let equation_ = combined_group_point_nonce_ + (challenge * combined_group_point_);
+        let equation = equation_.unwrap();
+
+        assert_eq!(s1_s3_agg_sig.base_point_mul(), equation);
+
+        // Case #3 signer 2 & 3 produced.
+        let active_list = vec![signer_2_public, signer_3_public];
+        let index_list = lagrance_index_list(&full_list, &active_list).unwrap();
+
+        let s2_index = lagrance_index(&full_list, signer_2_public).unwrap();
+        let s2_lagrance = interpolating_value(&index_list, s2_index).unwrap();
+
+        let s3_index = lagrance_index(&full_list, signer_3_public).unwrap();
+        let s3_lagrance = interpolating_value(&index_list, s3_index).unwrap();
+
+        let s2_s3_agg_sig_ = (s2_partial_sig * s2_lagrance) + (s3_partial_sig * s3_lagrance);
+        let s2_s3_agg_sig = s2_s3_agg_sig_.unwrap();
+
+        let equation_ = combined_group_point_nonce_ + (challenge * combined_group_point_);
+        let equation = equation_.unwrap();
+
+        assert_eq!(s2_s3_agg_sig.base_point_mul(), equation);
 
         Ok(())
     }
