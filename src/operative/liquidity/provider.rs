506d43e1af6f298b69050ef40229c174ef3f191f
brqgoo <100725376+brqgoo@users.noreply.github.com>
2025-01-15 13:21:56 +0300

    Update provider.rs

--- src/operative/liquidity/provider.rs
+++ src/operative/liquidity/provider.rs
@@ -4,3 +4,15 @@ pub fn provider_list() -> Vec<[u8; 32]> {
     // TODO..
     baked::OPERATOR_SET.to_vec()
 }
+
+pub fn is_valid_subset(keys: &Vec<[u8; 32]>) -> bool {
+    let provider_list = provider_list();
+
+    for key in keys {
+        if !provider_list.contains(key) {
+            return false;
+        }
+    }
+
+    true
+}
