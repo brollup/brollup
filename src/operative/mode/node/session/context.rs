f2a79ab87c9c89e2de19aa12f6395c02746a9bd1
brqgoo <100725376+brqgoo@users.noreply.github.com>
2025-02-15 12:24:37 -0500

    NSessionCtx

--- src/operative/mode/node/session/context.rs
+++ src/operative/mode/node/session/context.rs
@@ -1,13 +1,22 @@
 use crate::{
-    entry::{liftup::Liftup, recharge::Recharge},
+    entry::{call::Call, liftup::Liftup, recharge::Recharge, reserved::Reserved, vanilla::Vanilla},
     valtype::account::Account,
 };
 use secp::Scalar;
+use serde::{Deserialize, Serialize};
 
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
+pub enum MainEntry {
+    Vanilla(Vanilla),
+    Call(Call),
+}
+
+#[derive(Clone, Serialize, Deserialize)]
 pub struct NSessionCtx {
     account: Account,
     secret_key: Scalar,
     liftup: Option<Liftup>,
     recharge: Option<Recharge>,
+    main: MainEntry,
+    reserved: Option<Reserved>,
 }
