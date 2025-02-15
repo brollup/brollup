4ffe7e4630e778c1eac72689d60d50c7e263d4e5
brqgoo <100725376+brqgoo@users.noreply.github.com>
2025-02-15 12:00:57 -0500

    Liftup ⬆️

---
+++ src/constructive/entry/liftup.rs
@@ -0,0 +1,28 @@
+use crate::txo::lift::Lift;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Liftup {
+    lift_prevtxos: Vec<Lift>,
+}
+
+impl Liftup {
+    pub fn new(lifts: Vec<Lift>) -> Option<Liftup> {
+        let mut lift_prevtxos = Vec::<Lift>::new();
+
+        for lift in lifts.iter() {
+            match lift.outpoint() {
+                Some(_) => lift_prevtxos.push(lift.to_owned()),
+                None => return None,
+            }
+        }
+
+        let liftup = Liftup { lift_prevtxos };
+
+        Some(liftup)
+    }
+
+    pub fn lifts(&self) -> Vec<Lift> {
+        self.lift_prevtxos.clone()
+    }
+}
---
+++ src/constructive/entry/mod.rs
@@ -0,0 +1,2 @@
+pub mod liftup;
+pub mod recharge;
