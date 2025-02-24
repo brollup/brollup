e2b575e2599cc47e22733b3c05e61ed24524327c
brqgoo <100725376+brqgoo@users.noreply.github.com>
2025-02-24 14:18:31 -0500

    OSessionOpCovAck

--- src/operative/session/opcov/opcovack.rs
+++ src/operative/session/opcov/opcovack.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 
 /// `OSessionOpCovAck` is similar to `NSessionUphold`, but it is for returning
 /// partial covenant signatures from the operators rather than individual msg.senders.
-/// `OSessionOpCovAck` is returned by the operators to the coordinator upon receiving 
+/// `OSessionOpCovAck` is returned by the operators to the coordinator upon receiving
 /// `CSessionOpCov` from the coordinator.
 #[derive(Clone, Serialize, Deserialize)]
 pub struct OSessionOpCovAck {
@@ -21,4 +21,48 @@ pub struct OSessionOpCovAck {
     lift_prevtxo_partial_sigs: HashMap<Account, HashMap<Lift, Scalar>>,
     // Connector partial sigs
     connector_txo_partial_sigs: HashMap<Account, Vec<Scalar>>,
+}
+
+impl OSessionOpCovAck {
+    pub fn new(
+        payload_auth_partial_sig: Scalar,
+        vtxo_projector_partial_sig: Option<Scalar>,
+        connector_projector_partial_sig: Option<Scalar>,
+        zkp_contingent_partial_sig: Option<Scalar>,
+        lift_prevtxo_partial_sigs: HashMap<Account, HashMap<Lift, Scalar>>,
+        connector_txo_partial_sigs: HashMap<Account, Vec<Scalar>>,
+    ) -> OSessionOpCovAck {
+        OSessionOpCovAck {
+            payload_auth_partial_sig,
+            vtxo_projector_partial_sig,
+            connector_projector_partial_sig,
+            zkp_contingent_partial_sig,
+            lift_prevtxo_partial_sigs,
+            connector_txo_partial_sigs,
+        }
+    }
+
+    pub fn payload_auth_partial_sig(&self) -> Scalar {
+        self.payload_auth_partial_sig.clone()
+    }
+
+    pub fn vtxo_projector_partial_sig(&self) -> Option<Scalar> {
+        self.vtxo_projector_partial_sig.clone()
+    }
+
+    pub fn connector_projector_partial_sig(&self) -> Option<Scalar> {
+        self.connector_projector_partial_sig.clone()
+    }
+
+    pub fn zkp_contingent_partial_sig(&self) -> Option<Scalar> {
+        self.zkp_contingent_partial_sig.clone()
+    }
+
+    pub fn lift_prevtxo_partial_sigs(&self) -> HashMap<Account, HashMap<Lift, Scalar>> {
+        self.lift_prevtxo_partial_sigs.clone()
+    }
+
+    pub fn connector_txo_partial_sigs(&self) -> HashMap<Account, Vec<Scalar>> {
+        self.connector_txo_partial_sigs.clone()
+    }
 }
