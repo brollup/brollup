0e27289aa9d3065d9c17e678ac497f8b5ca241eb
brqgoo <100725376+brqgoo@users.noreply.github.com>
2025-03-06 08:05:39 +0300

    Update entry.rs

--- src/constructive/entry/entry.rs
+++ src/constructive/entry/entry.rs
@@ -66,15 +66,15 @@ pub enum UppermostRightBranch {
 #[derive(Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Entry {
     account: Account,
-    uppermost_left_branch: UppermostLeftBranch,
-    uppermost_right_branch: UppermostRightBranch,
+    uppermost_left_branch: Option<UppermostLeftBranch>,
+    uppermost_right_branch: Option<UppermostRightBranch>,
 }
 
 impl Entry {
     fn new(
         account: Account,
-        uppermost_left_branch: UppermostLeftBranch,
-        uppermost_right_branch: UppermostRightBranch,
+        uppermost_left_branch: Option<UppermostLeftBranch>,
+        uppermost_right_branch: Option<UppermostRightBranch>,
     ) -> Entry {
         Self {
             account,
@@ -83,14 +83,35 @@ impl Entry {
         }
     }
 
+    // Liftup and/or recharge ONLY.
+    pub fn new_nop(account: Account, liftup: Option<Liftup>, recharge: Option<Recharge>) -> Entry {
+        let uppermost_left_branch = {
+            if liftup.is_some() || recharge.is_some() {
+                Some(UppermostLeftBranch::new(liftup, recharge))
+            } else {
+                None
+            }
+        };
+
+        let uppermost_right_branch = None;
+        Self::new(account, uppermost_left_branch, uppermost_right_branch)
+    }
+
     pub fn new_move(
         account: Account,
         r#move: Move,
         liftup: Option<Liftup>,
         recharge: Option<Recharge>,
     ) -> Entry {
-        let uppermost_left_branch = UppermostLeftBranch::new(liftup, recharge);
-        let uppermost_right_branch = UppermostRightBranch::Transact(Transact::Move(r#move));
+        let uppermost_left_branch = {
+            if liftup.is_some() || recharge.is_some() {
+                Some(UppermostLeftBranch::new(liftup, recharge))
+            } else {
+                None
+            }
+        };
+
+        let uppermost_right_branch = Some(UppermostRightBranch::Transact(Transact::Move(r#move)));
         Self::new(account, uppermost_left_branch, uppermost_right_branch)
     }
 
@@ -100,8 +121,14 @@ impl Entry {
         liftup: Option<Liftup>,
         recharge: Option<Recharge>,
     ) -> Entry {
-        let uppermost_left_branch = UppermostLeftBranch::new(liftup, recharge);
-        let uppermost_right_branch = UppermostRightBranch::Transact(Transact::Call(call));
+        let uppermost_left_branch = {
+            if liftup.is_some() || recharge.is_some() {
+                Some(UppermostLeftBranch::new(liftup, recharge))
+            } else {
+                None
+            }
+        };
+        let uppermost_right_branch = Some(UppermostRightBranch::Transact(Transact::Call(call)));
         Self::new(account, uppermost_left_branch, uppermost_right_branch)
     }
 
@@ -111,10 +138,16 @@ impl Entry {
         liftup: Option<Liftup>,
         recharge: Option<Recharge>,
     ) -> Entry {
-        let uppermost_left_branch = UppermostLeftBranch::new(liftup, recharge);
-        let uppermost_right_branch = UppermostRightBranch::UpperRightBranch(
+        let uppermost_left_branch = {
+            if liftup.is_some() || recharge.is_some() {
+                Some(UppermostLeftBranch::new(liftup, recharge))
+            } else {
+                None
+            }
+        };
+        let uppermost_right_branch = Some(UppermostRightBranch::UpperRightBranch(
             UpperRightBranch::Liquidity(Liquidity::Add(add)),
-        );
+        ));
         Self::new(account, uppermost_left_branch, uppermost_right_branch)
     }
 
@@ -124,10 +157,16 @@ impl Entry {
         liftup: Option<Liftup>,
         recharge: Option<Recharge>,
     ) -> Entry {
-        let uppermost_left_branch = UppermostLeftBranch::new(liftup, recharge);
-        let uppermost_right_branch = UppermostRightBranch::UpperRightBranch(
+        let uppermost_left_branch = {
+            if liftup.is_some() || recharge.is_some() {
+                Some(UppermostLeftBranch::new(liftup, recharge))
+            } else {
+                None
+            }
+        };
+        let uppermost_right_branch = Some(UppermostRightBranch::UpperRightBranch(
             UpperRightBranch::Liquidity(Liquidity::Remove(remove)),
-        );
+        ));
         Self::new(account, uppermost_left_branch, uppermost_right_branch)
     }
 
@@ -137,10 +176,16 @@ impl Entry {
         liftup: Option<Liftup>,
         recharge: Option<Recharge>,
     ) -> Entry {
-        let uppermost_left_branch = UppermostLeftBranch::new(liftup, recharge);
-        let uppermost_right_branch = UppermostRightBranch::UpperRightBranch(
+        let uppermost_left_branch = {
+            if liftup.is_some() || recharge.is_some() {
+                Some(UppermostLeftBranch::new(liftup, recharge))
+            } else {
+                None
+            }
+        };
+        let uppermost_right_branch = Some(UppermostRightBranch::UpperRightBranch(
             UpperRightBranch::RightBranch(RightBranch::Swapout(swapout)),
-        );
+        ));
         Self::new(account, uppermost_left_branch, uppermost_right_branch)
     }
 
@@ -150,10 +195,16 @@ impl Entry {
         liftup: Option<Liftup>,
         recharge: Option<Recharge>,
     ) -> Entry {
-        let uppermost_left_branch = UppermostLeftBranch::new(liftup, recharge);
-        let uppermost_right_branch = UppermostRightBranch::UpperRightBranch(
+        let uppermost_left_branch = {
+            if liftup.is_some() || recharge.is_some() {
+                Some(UppermostLeftBranch::new(liftup, recharge))
+            } else {
+                None
+            }
+        };
+        let uppermost_right_branch = Some(UppermostRightBranch::UpperRightBranch(
             UpperRightBranch::RightBranch(RightBranch::Reserved(reserved)),
-        );
+        ));
         Self::new(account, uppermost_left_branch, uppermost_right_branch)
     }
 
@@ -162,28 +213,45 @@ impl Entry {
     }
 
     pub fn liftup(&self) -> Option<Liftup> {
-        self.uppermost_left_branch.liftup()
+        let uppermost_left_branch = match &self.uppermost_left_branch {
+            Some(uppermost_left_branch) => uppermost_left_branch,
+            None => return None,
+        };
+
+        uppermost_left_branch.liftup()
     }
 
     pub fn recharge(&self) -> Option<Recharge> {
-        self.uppermost_left_branch.recharge()
+        let uppermost_left_branch = match &self.uppermost_left_branch {
+            Some(uppermost_left_branch) => uppermost_left_branch,
+            None => return None,
+        };
+
+        uppermost_left_branch.recharge()
     }
 
-    pub fn main_combinator(&self) -> Combinator {
-        match &self.uppermost_right_branch {
+    pub fn main_combinator(&self) -> Option<Combinator> {
+        let uppermost_right_branch = match &self.uppermost_right_branch {
+            Some(uppermost_right_branch) => uppermost_right_branch,
+            None => return None,
+        };
+
+        match uppermost_right_branch {
             UppermostRightBranch::Transact(transact) => match transact {
-                Transact::Move(r#move) => Combinator::Move(r#move.clone()),
-                Transact::Call(call) => Combinator::Call(call.clone()),
+                Transact::Move(r#move) => Some(Combinator::Move(r#move.clone())),
+                Transact::Call(call) => Some(Combinator::Call(call.clone())),
             },
             UppermostRightBranch::UpperRightBranch(upper_right_branch) => {
                 match upper_right_branch {
                     UpperRightBranch::Liquidity(liquidity) => match liquidity {
-                        Liquidity::Add(add) => Combinator::Add(add.clone()),
-                        Liquidity::Remove(remove) => Combinator::Remove(remove.clone()),
+                        Liquidity::Add(add) => Some(Combinator::Add(add.clone())),
+                        Liquidity::Remove(remove) => Some(Combinator::Remove(remove.clone())),
                     },
                     UpperRightBranch::RightBranch(right_branch) => match right_branch {
-                        RightBranch::Swapout(swapout) => Combinator::Swapout(swapout.clone()),
-                        RightBranch::Reserved(reserved) => Combinator::Reserved(reserved.clone()),
+                        RightBranch::Swapout(swapout) => Some(Combinator::Swapout(swapout.clone())),
+                        RightBranch::Reserved(reserved) => {
+                            Some(Combinator::Reserved(reserved.clone()))
+                        }
                     },
                 }
             }
@@ -200,31 +268,62 @@ impl Entry {
     pub fn validate_account(&self) -> bool {
         let account = self.account();
 
-        if let Some(liftup) = &self.uppermost_left_branch.liftup {
-            if !liftup.validate_account(account) {
+        if let None = self.uppermost_left_branch {
+            if let None = self.uppermost_right_branch {
                 return false;
             }
         }
 
-        if let Some(recharge) = &self.uppermost_left_branch.recharge {
-            if !recharge.validate_account(account) {
-                return false;
+        if let Some(uppermost_left_branch) = &self.uppermost_left_branch {
+            if let Some(liftup) = &uppermost_left_branch.liftup {
+                if !liftup.validate_account(account) {
+                    return false;
+                }
+            }
+
+            if let Some(recharge) = &uppermost_left_branch.recharge {
+                if !recharge.validate_account(account) {
+                    return false;
+                }
             }
         }
 
-        let main_combinator = self.main_combinator();
-        match main_combinator {
-            Combinator::Move(r#move) => r#move.validate_account(account),
-            Combinator::Call(call) => call.validate_account(account),
-            Combinator::Add(add) => add.validate_account(account),
-            Combinator::Remove(remove) => remove.validate_account(account),
-            Combinator::Swapout(swapout) => swapout.validate_account(account),
-            // Reserved fails the validation.
-            Combinator::Reserved(_) => return false,
-            // Main combinator cannot be of liftup or recharge.
-            Combinator::Liftup(_) => return false,
-            Combinator::Recharge(_) => return false,
+        if let Some(main_combinator) = self.main_combinator() {
+            match main_combinator {
+                Combinator::Move(r#move) => {
+                    if !r#move.validate_account(account) {
+                        return false;
+                    }
+                }
+                Combinator::Call(call) => {
+                    if !call.validate_account(account) {
+                        return false;
+                    }
+                }
+                Combinator::Add(add) => {
+                    if !add.validate_account(account) {
+                        return false;
+                    }
+                }
+                Combinator::Remove(remove) => {
+                    if !remove.validate_account(account) {
+                        return false;
+                    }
+                }
+                Combinator::Swapout(swapout) => {
+                    if !swapout.validate_account(account) {
+                        return false;
+                    }
+                }
+                // Reserved fails the validation.
+                Combinator::Reserved(_) => return false,
+                // Main combinator cannot be of liftup or recharge.
+                Combinator::Liftup(_) => return false,
+                Combinator::Recharge(_) => return false,
+            }
         }
+
+        true
     }
 }
 
@@ -235,36 +334,48 @@ impl Sighash for Entry {
         // Account
         preimage.extend(self.account.key().serialize_xonly());
 
-        // Liftup
-        match &self.uppermost_left_branch.liftup {
-            Some(liftup) => {
+        match &self.uppermost_left_branch {
+            Some(uppermost_left_branch) => {
                 preimage.push(0x01);
-                preimage.extend(liftup.sighash());
+
+                match &uppermost_left_branch.liftup {
+                    Some(liftup) => {
+                        preimage.push(0x01);
+                        preimage.extend(liftup.sighash());
+                    }
+                    None => preimage.push(0x00),
+                }
+
+                match &uppermost_left_branch.recharge {
+                    Some(recharge) => {
+                        preimage.push(0x01);
+                        preimage.extend(recharge.sighash());
+                    }
+                    None => preimage.push(0x00),
+                }
             }
             None => preimage.push(0x00),
         }
 
-        // Recharge
-        match &self.uppermost_left_branch.recharge {
-            Some(recharge) => {
+        match &self.uppermost_right_branch {
+            Some(uppermost_right_branch) => {
                 preimage.push(0x01);
-                preimage.extend(recharge.sighash());
+
+                match &self.main_combinator() {
+                    Some(main_combinator) => match main_combinator {
+                        Combinator::Move(r#move) => preimage.extend(r#move.sighash()),
+                        Combinator::Call(call) => preimage.extend(call.sighash()),
+                        Combinator::Add(add) => preimage.extend(add.sighash()),
+                        Combinator::Remove(remove) => preimage.extend(remove.sighash()),
+                        Combinator::Swapout(swapout) => preimage.extend(swapout.sighash()),
+                        Combinator::Reserved(_) => return [0xffu8; 32],
+                        Combinator::Liftup(_) => return [0xffu8; 32],
+                        Combinator::Recharge(_) => return [0xffu8; 32],
+                    },
+                    None => return [0xffu8; 32],
+                }
             }
             None => preimage.push(0x00),
-        }
-
-        let main_combinator = self.main_combinator();
-        match main_combinator {
-            // Unexpected combinator types
-            Combinator::Liftup(_) => preimage.extend([0xffu8; 32]),
-            Combinator::Recharge(_) => preimage.extend([0xffu8; 32]),
-            // Main combinator types
-            Combinator::Move(r#move) => preimage.extend(r#move.sighash()),
-            Combinator::Call(call) => preimage.extend(call.sighash()),
-            Combinator::Add(add) => preimage.extend(add.sighash()),
-            Combinator::Remove(remove) => preimage.extend(remove.sighash()),
-            Combinator::Swapout(swapout) => preimage.extend(swapout.sighash()),
-            Combinator::Reserved(_) => preimage.extend([0xffu8; 32]),
         }
 
         preimage.hash(Some(HashTag::SighashEntry))
